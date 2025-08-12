b4303eb127ab4ec9cc5acc60a694843917417f43
kasparma <kaspar01991@gmail.com>
2025-08-12 15:35:27 +0530

    Find Subset

---
+++ src/main/java/com/kas/practice_javadsa/aug_2025/Backtaracking/P_5_SubsetWithRec.java
@@ -0,0 +1,28 @@
+package com.kas.practice_javadsa.aug_2025.Backtaracking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class P_5_SubsetWithRec {
+    static List<String> subsets = new ArrayList<>();
+    StringBuilder subset = new StringBuilder();
+    public void findSubset(String inp, int ind){
+        if(ind == inp.length()) {
+            subsets.add(subset.toString());
+
+            return;
+        }
+        subset.append(inp.charAt(ind));
+        findSubset(inp,ind+1);
+
+        //not include
+        subset.deleteCharAt(subset.length()-1);
+        findSubset(inp,ind+1);
+    }
+
+    public static void main(String[] args) {
+        String inp = "abc";
+        new P_5_SubsetWithRec().findSubset(inp,0);
+        System.out.println(subsets);
+    }
+}
